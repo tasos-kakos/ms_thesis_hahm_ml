c6a0b05d7b1ed65a72ed4f929e26e145bb524aba
Anastasios Kakos <tasos.kakos98@gmail.com>
2025-10-17 21:55:39 +0300

    Updated MdtNtupleDumper_muonTester.py
    Modified background sample dumper script to work efficiently outside of lxplus

--- dataProcessing/MdtNtupleDumper_muonTester.py
+++ dataProcessing/MdtNtupleDumper_muonTester.py
@@ -1,4 +1,5 @@
 import ROOT
+ROOT.gSystem.Load('libRVecSignedChar.so') # Load necessary dictionaries
 import numpy as np
 import h5py
 import cppyy
@@ -27,39 +28,48 @@ def process_events(input_path, output_path, max_events=10, onlyOneSector=True):
     for i, rvec in enumerate(data['MdtChamHitId_stationEta']):
         if i > 100000:
             break
+        if not (i%1000): # Print index every 1k values
+            print("___________________________________ NEW EVENT MDT = ", i, " _____________________________________")
+        
         event={}
         mask = np.ones(len(data['MdtChamHitId_tube'][i]), dtype=bool)
         mask[np.isin(data['MdtChamHit_driftR'][i], data['trkMdt_driftR'][i])] = False
     
         station_eta = data['MdtChamHitId_stationEta'][i]
-        station_eta_signed_int = np.array([int.from_bytes(x.encode('latin-1'), 'big',signed=True) for x in station_eta], dtype=np.int32)
+        arr = np.array(list(station_eta), dtype=np.int8)
+        station_eta_signed_int = arr.tolist()
         mask_station_eta = np.isin(np.asarray(station_eta_signed_int), [-7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7])
+        
         station_index = data['MdtChamHitId_stationIndex'][i]
-        station_index = np.array([ord(x) if isinstance(x, str) else x for x in station_index])
+        station_index = station_index.view(np.uint8)
         mask_station_index = np.isin(np.asarray(station_index), [0, 1, 2, 3, 4, 5])
 
         station_phi = data['MdtChamHitId_stationPhi'][i]
-        station_phi = [int.from_bytes(x.encode(), 'big') for x in station_phi]    
+        station_phi = station_phi.view(np.uint8)  
         mask_station_phi = np.isin(np.asarray(station_phi), [1, 2, 3, 4, 5, 6, 7, 8])
 
         final_mask = mask_station_eta & mask_station_phi & mask_station_index & mask
         
-        event_variables = ['MdtChamHitId_multiLayer', 'MdtChamHitId_stationEta', 'MdtChamHitId_stationPhi', 'MdtChamHitId_stationIndex', 'MdtChamHitId_tubeLayer', 'MdtChamHitId_tube', 'MdtChamHit_Adc', 'MdtChamHit_Tdc', 'MdtChamHit_ChamberLink', 'MdtChamHit_driftR', 'MdtChamHit_uncertDriftR']
+        event_variables = ['MdtChamHitId_multiLayer', 'MdtChamHitId_stationEta', 'MdtChamHitId_stationPhi', 'MdtChamHitId_stationIndex', 'MdtChamHitId_tubeLayer', 'MdtChamHitId_tube']
 
         for name in event_variables:
             name_dict = name.split("_")[-1]
             values = data[name][i]
-            if type(values) == cppyy.gbl.ROOT.VecOps.RVec['signed char']:
-                values = [int.from_bytes(x.encode('latin-1'), 'big', signed=True) for x in values]
-            if type(values) == cppyy.gbl.ROOT.VecOps.RVec['unsigned char']:
-                values = [int.from_bytes(x.encode(), 'little') for x in values]
+            if type(values) == type(data['MdtChamHitId_stationEta'][i]):
+                arr_values = np.array(list(values), dtype=np.int8)
+                values = arr_values.tolist()
+            if type(values) == type(data['MdtChamHitId_stationIndex'][i]):
+                values = values.view(np.uint8)
             if onlyOneSector:
                 filtered_values = np.asarray(values)[final_mask]
                 event[name_dict] = np.asarray(filtered_values)
             else:
                 event[name_dict] = np.asarray(values)
-        
-        event['eventNumber'] = int(data['eventNumber'][i])
+
+        driftR = data['MdtChamHit_driftR'][i]
+        filtered_values = np.asarray(driftR)[final_mask]
+        event['driftR'] = np.asarray(filtered_values)
+        event['eventNumber'] = data['eventNumber'][i]
     
         isMuon = np.zeros(len(filtered_values), dtype=int)
         event["isMuon"]=np.asarray(isMuon)
